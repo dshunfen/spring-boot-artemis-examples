f00da80ee1510c194433660ec78b3490c7471cda
David Shunfenthal <dshunfenthal@mindovermachines.com>
2018-01-20 21:48:01 -0500

    Fixing the undesirably autoconfigured connection factory and adding in a latch to wait for the received message before completing

--- hello-world/src/main/java/com/example/spring/artemis/hello/HelloWorldExample.java
+++ hello-world/src/main/java/com/example/spring/artemis/hello/HelloWorldExample.java
@@ -1,31 +1,28 @@
 package com.example.spring.artemis.hello;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
 
 /**
  * A simple JMS Queue Hello World example.
  */
 @SpringBootApplication
 public class HelloWorldExample {
-
-	@JmsListener(destination = "exampleQueue")
-	public void messageConsumer(String text) {
-		// Step 5. Receive the message
-		System.out.println("Received message: " + text);
-	}
-
+	
 	public static void main(String[] args) throws Exception {
 
 		// Step 1. Instantiate the Spring Boot context
 		ConfigurableApplicationContext context = SpringApplication.run(HelloWorldExample.class);
 
 		// Step 2. Create an example text message
-		// Note: Spring will perform the conversion operations behind the scenes
-		// To turn this into a JMS TextMessage
 		String text = "This is a text message";
 
 		// Step 3. Retrieve the Spring Boot autoconfigured JmsTemplate from the context
@@ -35,8 +32,36 @@ public class HelloWorldExample {
 		messageProducer.convertAndSend("exampleQueue", text);
 
 		System.out.println("Sent message: " + text);
+		
+		// Step 5. Wait until we've received the message
+		context.getBean(MessageConsumer.class).getLatch().await();
 
 		// Step 6. Close the context so things get cleaned up properly and the test ends
 		context.close();
+	}
+	
+	@Bean
+	public ActiveMQConnectionFactory connectionFactory() {
+		String testUri = "tcp://localhost:61616";
+		ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(testUri);
+		return cf;
+	}
+
+	@Component
+	public class MessageConsumer {
+
+		private CountDownLatch latch = new CountDownLatch(1);
+
+		@JmsListener(destination = "exampleQueue")
+		public void messageConsumer(String text) {
+			// Step 5. Receive the message and release waiting latch
+			System.out.println("Received message: " + text);
+			latch.countDown();
+		}
+		
+		public CountDownLatch getLatch() {
+			return latch;
+		}
+
 	}
 }
--- ssl-enabled/src/main/java/com/example/spring/artemis/ssl/SSLExample.java
+++ ssl-enabled/src/main/java/com/example/spring/artemis/ssl/SSLExample.java
@@ -1,31 +1,28 @@
 package com.example.spring.artemis.ssl;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
 
 /**
  * A simple JMS Queue example that uses SSL secure transport.
  */
 @SpringBootApplication
 public class SSLExample {
-
-	@JmsListener(destination = "exampleQueue")
-	public void messageConsumer(String text) {
-		// Step 5. Receive the message
-		System.out.println("Received message: " + text);
-	}
-
+	
 	public static void main(String[] args) throws Exception {
 
 		// Step 1. Instantiate the Spring Boot context
 		ConfigurableApplicationContext context = SpringApplication.run(SSLExample.class);
 
 		// Step 2. Create an example text message
-		// Note: Spring will perform the conversion operations behind the scenes
-		// To turn this into a JMS TextMessage
 		String text = "This is a text message";
 
 		// Step 3. Retrieve the Spring Boot autoconfigured JmsTemplate from the context
@@ -35,8 +32,36 @@ public class SSLExample {
 		messageProducer.convertAndSend("exampleQueue", text);
 
 		System.out.println("Sent message: " + text);
+		
+		// Step 5. Wait until we've received the message
+		context.getBean(MessageConsumer.class).getLatch().await();
 
 		// Step 6. Close the context so things get cleaned up properly and the test ends
 		context.close();
+	}
+	
+	@Bean
+	public ActiveMQConnectionFactory connectionFactory() {
+		String testUri = "tcp://localhost:5500?sslEnabled=true&amp;trustStorePath=activemq/server0/activemq.example.truststore&amp;trustStorePassword=activemqexample";
+		ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(testUri);
+		return cf;
+	}
+
+	@Component
+	public class MessageConsumer {
+
+		private CountDownLatch latch = new CountDownLatch(1);
+
+		@JmsListener(destination = "exampleQueue")
+		public void messageConsumer(String text) {
+			// Step 5. Receive the message and release waiting latch
+			System.out.println("Received message: " + text);
+			latch.countDown();
+		}
+		
+		public CountDownLatch getLatch() {
+			return latch;
+		}
+
 	}
 }
